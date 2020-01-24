9f15815fdc8ed33bcab8cdca2174e67e785fb629
evdtooling <60244449+evdtooling@users.noreply.github.com>
2020-01-24 18:45:04 +0100

    master

---
+++ MyPluginControl.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Tooling.Connector;
+using System.Xml;
+
+namespace Field_Seeker
+{
+    public partial class MyPluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private void searchField_Click(object sender, EventArgs e)
+        {
+            ExecuteMethod(SearchField);
+        }
+        private void SearchField()
+        {
+            if (fieldName.TextLength > 0)
+            {
+                string fieldname = fieldName.Text.ToLower();
+
+                RetrieveAllEntitiesRequest metaDataRequest = new RetrieveAllEntitiesRequest();
+                metaDataRequest.EntityFilters = EntityFilters.Attributes;
+
+                WorkAsync(new WorkAsyncInfo
+                {
+                    Message = "Searching field in all entities...",
+                    Work = (worker, args) =>
+                    {
+
+                        args.Result = (RetrieveAllEntitiesResponse)Service.Execute(metaDataRequest);
+                    },
+                    PostWorkCallBack = (args) =>
+                    {
+                        if (args.Error != null)
+                        {
+                            MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                        var result = args.Result as RetrieveAllEntitiesResponse;
+                        if (result != null)
+                        {
+                            //Create an entitycounter and retrieve the EntityMetadata
+                            int counter = 0;
+                            var entities = result.EntityMetadata;
+                            //Clear the TreeView before refill
+                            resultsTree.Nodes.Clear();
+
+                            foreach (var entity in entities)
+                            {
+                                string entityname = entity.SchemaName.ToLower();
+                                foreach (AttributeMetadata att in entity.Attributes)
+                                {
+                                    string attributename = att.SchemaName.ToLower();
+                                    if (attributename == fieldname)
+                                    {
+                                        //Add the Entity the TreeView
+                                        TreeNode parent = resultsTree.Nodes.Add(entityname);
+
+                                        //Add Type
+                                        resultsTree.Nodes[parent.Index].Nodes.Add("Type: " + att.AttributeType.Value.ToString());
+
+                                        //Add FieldType
+                                        int fieldtype = att.SourceType.Value;
+                                        string fieldtype_label = "Simple";
+                                        if(fieldtype == 0)
+                                        {
+                                            fieldtype_label = "Simple";
+                                        } else if (fieldtype == 1)
+                                        {
+                                            fieldtype_label = "Calculated";
+                                        } else if (fieldtype == 2)
+                                        {
+                                            fieldtype_label = "Rollup";
+                                        }
+                                        resultsTree.Nodes[parent.Index].Nodes.Add("Field Type: " + fieldtype_label);
+
+                                        //Add RequiredLevel
+                                        resultsTree.Nodes[parent.Index].Nodes.Add("Field Requirement: " + att.RequiredLevel.Value.ToString());
+
+                                        //Add FieldSecurity
+                                        resultsTree.Nodes[parent.Index].Nodes.Add("Field Security: " + att.IsSecured.Value.ToString());
+
+                                        //Add AuditStatus
+                                        resultsTree.Nodes[parent.Index].Nodes.Add("Audit Status: " + att.IsAuditEnabled.Value.ToString());
+
+                                        //Count up for an attribute
+                                        counter++;
+                                    }
+                                }
+                            }
+                            fieldName.Text = fieldname;
+                            fieldName.Refresh();
+                            resultsTree.Refresh();
+
+                            if(counter == 0)
+                            {
+                                MessageBox.Show("The field has no been found.",
+                                "Field Not Found",
+                                MessageBoxButtons.OK);
+                            }
+                            else
+                            {
+                                MessageBox.Show("The field \"" + fieldname + "\" has been found in " + counter + " entities.",
+                                "Field Found",
+                                MessageBoxButtons.OK);
+                            }                            
+                        }
+                    }
+                });
+            }
+            else
+            {
+                MessageBox.Show("Please enter a field name and restart searching.",
+                "Field Missing",
+                MessageBoxButtons.OK);
+            }
+        }
+    }
+}
---
+++ MyPluginControl.designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Field_Seeker
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.searchField = new System.Windows.Forms.ToolStripButton();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.fieldNameLabel = new System.Windows.Forms.Label();
+            this.fieldName = new System.Windows.Forms.TextBox();
+            this.resultsTree = new System.Windows.Forms.TreeView();
+            this.toolStripMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.searchField});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStripMenu.Size = new System.Drawing.Size(1185, 31);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(107, 28);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // searchField
+            // 
+            this.searchField.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.searchField.Name = "searchField";
+            this.searchField.Size = new System.Drawing.Size(93, 28);
+            this.searchField.Text = "Search Field";
+            this.searchField.ToolTipText = "Search field in all entities.";
+            this.searchField.Click += new System.EventHandler(this.searchField_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(150, 41);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(55, 17);
+            this.resultLabel.TabIndex = 6;
+            this.resultLabel.Text = "Results";
+            // 
+            // fieldNameLabel
+            // 
+            this.fieldNameLabel.AutoSize = true;
+            this.fieldNameLabel.Location = new System.Drawing.Point(12, 41);
+            this.fieldNameLabel.Name = "fieldNameLabel";
+            this.fieldNameLabel.Size = new System.Drawing.Size(79, 17);
+            this.fieldNameLabel.TabIndex = 7;
+            this.fieldNameLabel.Text = "Field Name";
+            // 
+            // fieldName
+            // 
+            this.fieldName.Location = new System.Drawing.Point(15, 61);
+            this.fieldName.Name = "fieldName";
+            this.fieldName.Size = new System.Drawing.Size(132, 22);
+            this.fieldName.TabIndex = 8;
+            // 
+            // resultsTree
+            // 
+            this.resultsTree.Location = new System.Drawing.Point(153, 61);
+            this.resultsTree.Name = "resultsTree";
+            this.resultsTree.Size = new System.Drawing.Size(357, 753);
+            this.resultsTree.TabIndex = 9;
+            // 
+            // MyPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.resultsTree);
+            this.Controls.Add(this.fieldName);
+            this.Controls.Add(this.fieldNameLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.toolStripMenu);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(1185, 835);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton searchField;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label fieldNameLabel;
+        private System.Windows.Forms.TextBox fieldName;
+        private System.Windows.Forms.TreeView resultsTree;
+    }
+}
